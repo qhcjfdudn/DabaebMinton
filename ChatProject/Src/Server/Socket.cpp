d7aeb9b8f334e1c71ed11392f318b47f6a39ba1c
장현진 <jhjhyjin@naver.com>
2025-01-14 16:59:58 +0900

    [feat] create TCP Socket class

---
+++ ChatProject/Src/Server/Socket.cpp
@@ -0,0 +1,22 @@
+#include "ServerPCH.h"
+#include "Socket.h"
+
+SOCKET Socket::CreateWSASocket()
+{
+	return WSASocket(
+		AF_INET,
+		SOCK_STREAM,
+		0,
+		nullptr,
+		0,
+		WSA_FLAG_OVERLAPPED);
+}
+
+int Socket::BindServer(unsigned int port) {
+	sockaddr_in s_in = {};
+	s_in.sin_family = AF_INET;
+	s_in.sin_addr.S_un.S_addr = INADDR_ANY;
+	s_in.sin_port = htons(port);
+
+	return bind(m_socket, reinterpret_cast<sockaddr*>(&s_in), sizeof(s_in));
+}
---
+++ ChatProject/Src/Server/Socket.h
@@ -0,0 +1,16 @@
+#pragma once
+class Socket
+{
+public:
+	static const int MAX_RECEIVE_LENGTH = 8'192;
+	
+	static SOCKET CreateWSASocket();
+	
+	char m_receiveBuffer[MAX_RECEIVE_LENGTH + 1] = { 0, };
+	OVERLAPPED m_readOverlappedStruct{};
+	DWORD m_readFlags{};
+
+	SOCKET m_socket{};
+
+	int BindServer(unsigned int port);
+};
