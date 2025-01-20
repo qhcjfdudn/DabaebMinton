c33883d9d7bad291cc19f703238c2df2dd3ff7b3
장현진 <jhjhyjin@naver.com>
2025-01-21 00:15:35 +0900

    [feat] create SendQueue class

---
+++ ChatProject/Src/Server/SendQueue.cpp
@@ -0,0 +1,26 @@
+#include "ServerPCH.h"
+#include "SendQueue.h"
+
+SendQueue& SendQueue::GetInstance()
+{
+	static SendQueue sInstance;
+	return sInstance;
+}
+
+void SendQueue::Push(const string& str)
+{
+	_bufferQueue.push(str);
+}
+
+string SendQueue::Front()
+{
+	string& ret = _bufferQueue.front();
+	_bufferQueue.pop();
+
+	return ret;
+}
+
+bool SendQueue::Empty()
+{
+	return _bufferQueue.empty();
+}
