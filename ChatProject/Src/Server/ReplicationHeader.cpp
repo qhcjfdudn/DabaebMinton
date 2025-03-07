c80fdc5c2c655263632d2cb236e0ff15faa154bc
장현진 <jhjhyjin@naver.com>
2025-03-07 18:45:10 +0900

    [feat] add replicationHeader to packet

--- ChatProject/Src/Server/ReplicationHeader.cpp
+++ ChatProject/Src/Server/ReplicationHeader.cpp
@@ -3,7 +3,7 @@
 
 void ReplicationHeader::Write(OutputMemoryBitStream& inStream)
 {
-	inStream.WriteBits(&_ra, static_cast<int>(ReplicationAction::Size));
+	inStream.WriteBits(&_ra, static_cast<int>(ReplicationAction::RA_Max));
 	inStream.Write(_nid);
 	inStream.Write(_cid);
 }
