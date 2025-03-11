825df1196451a050719664fbd1750b6d7bc8bb98
장현진 <jhjhyjin@naver.com>
2025-03-11 18:13:21 +0900

    [fix] solve over bit size alloc bug

--- ChatProject/Src/Server/ReplicationHeader.cpp
+++ ChatProject/Src/Server/ReplicationHeader.cpp
@@ -1,9 +1,17 @@
 #include "ServerPCH.h"
 #include "ReplicationHeader.h"
 
+#include "GetRequiredBits.h"
+
 void ReplicationHeader::Write(OutputMemoryBitStream& inStream)
 {
-	inStream.WriteBits(&_ra, static_cast<int>(ReplicationAction::RA_Max));
+	cout << "inStream.GetBitLength(): " << inStream.GetBitLength() << endl;
+	inStream.WriteBits(&_ra, GetRequiredBits< static_cast<int>(ReplicationAction::RA_Max)>::value);
+	cout << "inStream.GetBitLength(): " << inStream.GetBitLength() << endl;
+	cout << "_nid: " << _nid << endl;
 	inStream.Write(_nid);
+	cout << "inStream.GetBitLength(): " << inStream.GetBitLength() << endl;
+	cout << "_cid: " << _cid << endl;
 	inStream.Write(_cid);
+	cout << "inStream.GetBitLength(): " << inStream.GetBitLength() << endl;
 }
