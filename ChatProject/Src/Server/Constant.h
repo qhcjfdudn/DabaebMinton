6352c93d08196ec0b3b8172ccdfbf3b1011b8997
장현진 <jhjhyjin@naver.com>
2025-03-31 18:21:27 +0900

    [feat] change packet period 2.5Hz -> 20Hz

--- ChatProject/Src/Server/Constant.h
+++ ChatProject/Src/Server/Constant.h
@@ -4,7 +4,7 @@ namespace Constant
 {
 #ifdef STAGING_ENV
 	const float FIXED_UPDATE_TIMESTEP		= 1.f;		// 1Hz
-	const float PACKET_PERIOD				= 0.4f;		// 2.5Hz
+	const float PACKET_PERIOD				= 0.05f;	// 20Hz
 	const float PHYSX_FIXED_UPDATE_TIMESTEP = 1 / 60.f;	// 60Hz
 #else
 	const float FIXED_UPDATE_TIMESTEP		= 0.02f;	// 50Hz
