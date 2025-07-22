6f0c79beaeefb8250178e539b8d4240e582f0d35
장현진 <jhjhyjin@naver.com>
2025-07-22 16:56:00 +0900

    [refactor] 사용하지 않는 코드 제거

--- client/DabaebMinton/Assets/Scripts/Badminton/BadmintonPenaltyArea.cs
+++ client/DabaebMinton/Assets/Scripts/Badminton/BadmintonPenaltyArea.cs
@@ -2,14 +2,4 @@ using UnityEngine;
 
 public class BadmintonPenaltyArea : MonoBehaviour
 {
-    private BadmintonPlayController _badmintonPlayController;
-
-    private void Start()
-    {
-        _badmintonPlayController = FindFirstObjectByType<BadmintonPlayController>();
-        if (_badmintonPlayController == null)
-        {
-            Debug.LogError($"[{gameObject.name}] GameManager not found in the scene.");
-        }
-    }
 }
