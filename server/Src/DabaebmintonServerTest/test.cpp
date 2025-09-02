eef5d203eac1b151299adc441889941d21012b74
장현진 <jhjhyjin@naver.com>
2025-09-02 14:55:32 +0900

    [feat] test Game Creation/Delete

--- server/Src/DabaebmintonServerTest/test.cpp
+++ server/Src/DabaebmintonServerTest/test.cpp
@@ -1,6 +1,45 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include <WinSock2.h>
+#include <mswsock.h>
+
+#include "PhysicsEngine.h"
+#include "GameManager.h"
+#include "ClientInfo.h"
+
+class GameCreation : public ::testing::Test {
+protected:
+	virtual void SetUp() override
+	{
+		physicsEngine.InitPhysics();
+	}
+	virtual void TearDown() override
+	{
+		physicsEngine.CleanupPhysics();
+	}
+
+	PhysicsEngine& physicsEngine = PhysicsEngine::GetInstance();
+};
+
+// Client 매칭이 성사되면 game을 만든다.
+TEST_F(GameCreation, IsNumberIncreasedAfterGameCreation)
+{
+	GameManager& gm = GameManager::GetInstance();
+
+	sockaddr_in s1{}, s2{};
+	ClientInfo c1{ 0, s1 }, c2{ 1, s2 };
+	
+	ULONG_PTR dummyGameKey = 0x123;
+	ULONG_PTR nonExistGameKey = 0x12356;
+
+	EXPECT_EQ(gm._games.size(), 0);
+
+	gm.CreateGame(dummyGameKey, &c1, &c2);
+	EXPECT_EQ(gm._games.size(), 1);
+
+	gm.RemoveGame(nonExistGameKey);
+	EXPECT_EQ(gm._games.size(), 1);
+
+	gm.RemoveGame(dummyGameKey);
+	EXPECT_EQ(gm._games.size(), 0);
 }
