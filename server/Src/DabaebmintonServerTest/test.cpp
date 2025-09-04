268493157d5d14bcdc8b25976cfb566b30b70c89
장현진 <jhjhyjin@naver.com>
2025-09-04 10:50:45 +0900

    [choir] Modify TestName and reorder code line

--- server/Src/DabaebmintonServerTest/test.cpp
+++ server/Src/DabaebmintonServerTest/test.cpp
@@ -22,21 +22,20 @@ protected:
 };
 
 // Client 매칭이 성사되면 game을 만든다.
-TEST_F(GameCreation, IsNumberIncreasedAfterGameCreation)
+TEST_F(GameCreation, CheckGamesSizeWhenGameCreatedAndDeleted)
 {
 	GameManager& gm = GameManager::GetInstance();
 
 	sockaddr_in s1{}, s2{};
 	ClientInfo c1{ 0, s1 }, c2{ 1, s2 };
 	
-	ULONG_PTR dummyGameKey = 0x123;
-	ULONG_PTR nonExistGameKey = 0x12356;
-
 	EXPECT_EQ(gm._games.size(), 0);
 
+	ULONG_PTR dummyGameKey = 0x123;
 	gm.CreateGame(dummyGameKey, &c1, &c2);
 	EXPECT_EQ(gm._games.size(), 1);
 
+	ULONG_PTR nonExistGameKey = 0x12356;
 	gm.RemoveGame(nonExistGameKey);
 	EXPECT_EQ(gm._games.size(), 1);
 
