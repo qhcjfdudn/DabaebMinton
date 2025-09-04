5f9adb3982797cca1e7bcacfa3686687b04f0aed
장현진 <jhjhyjin@naver.com>
2025-09-04 14:29:44 +0900

    [choir] remove unnecessary comment

--- server/Src/DabaebmintonServerCore/ReplicationHeader.cpp
+++ server/Src/DabaebmintonServerCore/ReplicationHeader.cpp
@@ -5,13 +5,7 @@
 
 void ReplicationHeader::Write(OutputMemoryBitStream& inStream)
 {
-	cout << "inStream.GetBitLength(): " << inStream.GetBitLength() << endl;
 	inStream.WriteBits(&_ra, GetRequiredBits(static_cast<int>(ReplicationAction::RA_Max)));
-	cout << "inStream.GetBitLength(): " << inStream.GetBitLength() << endl;
-	cout << "_nid: " << _nid << endl;
 	inStream.Write(_nid);
-	cout << "inStream.GetBitLength(): " << inStream.GetBitLength() << endl;
-	cout << "_cid: " << _cid << endl;
 	inStream.Write(_cid);
-	cout << "inStream.GetBitLength(): " << inStream.GetBitLength() << endl;
 }
