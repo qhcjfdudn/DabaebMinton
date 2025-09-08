0db4d89f9147ec48fe385e366efceae4524755e4
장현진 <jhjhyjin@naver.com>
2025-09-09 00:49:19 +0900

    [feat] Modify Maximum Packet Size

--- server/Src/DabaebmintonServerCore/Constant.h
+++ server/Src/DabaebmintonServerCore/Constant.h
@@ -13,6 +13,5 @@ namespace Constant
 	const float PHYSX_FIXED_UPDATE_TIMESTEP = 1 / 60.f;	// 60Hz
 	const float REPLICATION_PERIOD = 1 / 20.f; // 20Hz
 #endif
-
-	const int MAX_PACKET_SIZE				= 65'535 /* UDP total length */ - 8 /* UDP Header */ - 20 /* IP Header */;
+	const int MAX_PACKET_SIZE				= 1'500 /* MTU */ - 8 /* UDP Header */ - 20 /* IP Header */;
 }
