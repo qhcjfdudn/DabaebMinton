65f64db6986cf42739bf3b5e1a5a57889855958a
장현진 <jhjhyjin@naver.com>
2025-09-18 02:28:10 +0900

    [choir] remove unnecessary code - RUDPPacketizer

--- server/Src/DabaebmintonServerCore/Game.cpp
+++ server/Src/DabaebmintonServerCore/Game.cpp
@@ -6,7 +6,6 @@
 #include "InputMemoryBitStream.h"
 #include "Constant.h"
 #include "ClientInfo.h"
-#include "RUDPPacketizer.h"
 #include "Packet.h"
 #include "GameObject.h"
 
@@ -62,32 +61,4 @@ void Game::SendPacket()
 
 void Game::SendPacket(ClientInfo* client)
 {
-	// Packet Period가 되었을 때 호출. client별 0~2 channel의 데이터를 보낸다. 
-	// 0 channel: Unreliable. ReplicateUpdate.
-	// 1 channel: Reliable. ReplicateCreate/Delete.
-	// 2 channel: Reliable. RPC.
-
-	auto& networkManagerServer = NetworkManagerServer::GetInstance();
-	auto& rudpPacketizer = RUDPPacketizer::GetInstance();
-	const PacketType packetTypes[3] = { PacketType::PT_ReplicationData, PacketType::PT_ReplicationData, PacketType::PT_RPC };
-
-	// channel 0
-	int channelId = 0;
-	auto packets = rudpPacketizer.Packetize(channelId, client->m_sequenceNoInChannels[channelId], packetTypes[channelId], _level.gameObjects);
-	networkManagerServer.SendTo(client, packets);
-
-	// channel [1, MAX_CHANNEL_COUNT)
-	for (channelId = 1; channelId < Constant::RUDP_MAX_CHANNEL_SIZE; ++channelId)
-	{
-		OutputMemoryBitStream& outStream = client->m_pendingStreamToSendingInChannels[channelId];
-
-		if (outStream.GetBitLength() == 0)
-			continue;
-		
-		uint8_t& seqNum = client->m_sequenceNoInChannels[channelId];
-		packets = rudpPacketizer.PacketizeReliable(channelId, seqNum, packetTypes[channelId], outStream);
-		outStream.Clear();
-
-		networkManagerServer.SendTo(client, packets);
-	}
 }
