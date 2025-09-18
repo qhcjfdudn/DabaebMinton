dd86ba6eeaf01c4a10f42d4d02a82e9a5327c8e6
장현진 <jhjhyjin@naver.com>
2025-09-19 02:06:42 +0900

    [feat] modify ClientInfo class
    - add sockaddr_in struct that used for sendto() param(remote addr)
    - remove regacy RUDP code

--- server/Src/DabaebmintonServerCore/ClientInfo.cpp
+++ server/Src/DabaebmintonServerCore/ClientInfo.cpp
@@ -4,4 +4,9 @@
 ClientInfo::ClientInfo(const string& ip, const unsigned int port) :
 	_ipPort{ ip + ":" + std::to_string(port) }
 {
+	ZeroMemory(&_sendOverlappedDto, sizeof(_sendOverlappedDto));
+	ZeroMemory(&_sockAddr, sizeof(_sockAddr));
+	_sockAddr.sin_family = AF_INET;
+	_sockAddr.sin_addr.S_un.S_addr = inet_addr(ip.c_str());
+	_sockAddr.sin_port = htons(port);
 }
--- server/Src/DabaebmintonServerCore/ClientInfo.h
+++ server/Src/DabaebmintonServerCore/ClientInfo.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "Constant.h"
-#include "OutputMemoryBitStream.h"
 #include "OverlappedDto.h"
 #include "ReplicationManager.h"
 #include "DeliveryNotificationManager.h"
@@ -11,18 +9,20 @@ class ClientInfo
 public:
 	ClientInfo(const string& ip, const unsigned int port);
 
+	sockaddr_in* GetSockAddr() { return &_sockAddr; }
+
 	ReplicationManager& GetReplicationManager() { return _replicationManager; }
 	DeliveryNotificationManager& GetDeliveryNotificationManager() { return _deliveryNotificationManager; }
 
 	const std::string _ipPort;
 
-	// for RUDP + IOCP
-	uint8_t m_sequenceNoInChannels[Constant::RUDP_MAX_CHANNEL_SIZE] = {};
-	OverlappedDto m_OverlappedDtdo[Constant::RUDP_MAX_CHANNEL_SIZE] = {};
-	
-	OutputMemoryBitStream m_pendingStreamToSendingInChannels[Constant::RUDP_MAX_CHANNEL_SIZE] = {};
+	// for IOCP
+	OverlappedDto _sendOverlappedDto;
 
 private:
+	sockaddr_in _sockAddr;
+
+	// for RUDP
 	ReplicationManager _replicationManager{};
 	DeliveryNotificationManager _deliveryNotificationManager{};
 };
