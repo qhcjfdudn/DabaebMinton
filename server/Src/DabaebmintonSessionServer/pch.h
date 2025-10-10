15c2f32b73fb9cf4a4ed67e2e7ed7f4753ee28df
장현진 <jhjhyjin@naver.com>
2025-10-10 15:15:13 +0900

    [feat] modify SessionServer encoding
    - CP949 to utf-8

--- server/Src/DabaebmintonSessionServer/pch.h
+++ server/Src/DabaebmintonSessionServer/pch.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <unordered_map>
 #include <thread>
+#include <chrono>
+#include <random>
 
+#include <spdlog/spdlog.h>
+
+using namespace std::chrono;
+
+using std::unordered_map;
 using std::thread;
+
+using PlayerId_t = uint32_t;
