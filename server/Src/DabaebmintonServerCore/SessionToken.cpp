4413f5824f133954176aa043ba29579f07cfdb16
장현진 <jhjhyjin@naver.com>
2025-10-10 15:13:57 +0900

    [refactor] modify type uint64_t -> sessionId_t

--- server/Src/DabaebmintonServerCore/SessionToken.cpp
+++ server/Src/DabaebmintonServerCore/SessionToken.cpp
@@ -1,7 +1,7 @@
 #include "ServerPCH.h"
 #include "SessionToken.h"
 
-SessionToken::SessionToken(uint64_t tokenId, PlayerId_t playerId) :
+SessionToken::SessionToken(SessionId_t tokenId, PlayerId_t playerId) :
 	_tokenId{ tokenId },
 	_playerId{ playerId },
 	_lastActive{ steady_clock::now() },
--- server/Src/DabaebmintonServerCore/SessionToken.h
+++ server/Src/DabaebmintonServerCore/SessionToken.h
@@ -1,14 +1,17 @@
 #pragma once
+
+using SessionId_t = uint64_t;
+
 class SessionToken {
 public:
-	SessionToken(uint64_t tokenId, PlayerId_t playerId);
+	SessionToken(SessionId_t tokenId, PlayerId_t playerId);
 
-	uint64_t GetTokenId() const { return _tokenId; }
+	SessionId_t GetTokenId() const { return _tokenId; }
 	PlayerId_t GetPlayerId() const { return _playerId; }
 	void SetActive(bool isActive) { _isActive = isActive; }
 
 private:
-	uint64_t _tokenId;						// 클라이언트 인증용 토큰
+	SessionId_t _tokenId;						// 클라이언트 인증용 토큰
 	PlayerId_t _playerId;					// 이 세션의 소유자
 	steady_clock::time_point _lastActive;	// 마지막 활동 시간
 	//std::string secretKey;				// 패킷 HMAC/암호화용
