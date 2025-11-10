c71df5d85bdd06ba3b8f6b86be538606af7acb4b
장현진 <jhjhyjin@naver.com>
2025-11-10 18:19:40 +0900

    [choir] modify comment

--- server/Src/DabaebmintonServerCore/Constant.h
+++ server/Src/DabaebmintonServerCore/Constant.h
@@ -4,16 +4,13 @@ namespace Constant
 {
 #ifdef STAGING_ENV
 	constexpr float FIXED_UPDATE_TIMESTEP		= 1 / 1.f;	// 1Hz
-	constexpr float PACKET_PERIOD				= 1 / 2.f;	// 2Hz
 	constexpr float PHYSX_FIXED_UPDATE_TIMESTEP = 1 / 2.f;	// 2Hz
 	constexpr float REPLICATION_PERIOD			= 1 / 2.f;	// 2Hz
 #else
 	constexpr float FIXED_UPDATE_TIMESTEP		= 1 / 50.f;	// 50Hz
-	constexpr float PACKET_PERIOD				= 1 / 20.f;	// 20Hz
 	constexpr float PHYSX_FIXED_UPDATE_TIMESTEP = 1 / 50.f;	// 50Hz
 	constexpr float REPLICATION_PERIOD			= 1 / 20.f; // 20Hz
 #endif
-	constexpr uint16_t MAX_PACKET_SIZE		= 1'500 /* MTU */ - 8 /* UDP Header */ - 20 /* IP Header */;
+	constexpr uint16_t MAX_PACKET_SIZE		= 1'500 /* MTU */ - 20 /* IP Header */ - 8 /* UDP Header */;
 	constexpr float PACKET_TIME_OUT			= 0.5f;
-	constexpr size_t RUDP_MAX_CHANNEL_SIZE = 3;
 }
