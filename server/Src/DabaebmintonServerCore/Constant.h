c6afbf547b003b68df872a460de5ce1d05453146
장현진 <jhjhyjin@naver.com>
2025-11-11 10:52:28 +0900

    [feat] Update Constant
    - S/C 간 Replication Code 안정화로 Server FixedUpdate/Replication Period 증가

--- server/Src/DabaebmintonServerCore/Constant.h
+++ server/Src/DabaebmintonServerCore/Constant.h
@@ -4,12 +4,12 @@ namespace Constant
 {
 #ifdef STAGING_ENV
 	constexpr float FIXED_UPDATE_TIMESTEP		= 1 / 1.f;	// 1Hz
-	constexpr float PHYSX_FIXED_UPDATE_TIMESTEP = 1 / 2.f;	// 2Hz
-	constexpr float REPLICATION_PERIOD			= 1 / 2.f;	// 2Hz
+	constexpr float PHYSX_FIXED_UPDATE_TIMESTEP = 1 / 60.f;	// 60Hz
+	constexpr float REPLICATION_PERIOD			= 1 / 40.f;	// 40Hz
 #else
 	constexpr float FIXED_UPDATE_TIMESTEP		= 1 / 50.f;	// 50Hz
-	constexpr float PHYSX_FIXED_UPDATE_TIMESTEP = 1 / 50.f;	// 50Hz
-	constexpr float REPLICATION_PERIOD			= 1 / 20.f; // 20Hz
+	constexpr float PHYSX_FIXED_UPDATE_TIMESTEP = 1 / 60.f;	// 60Hz
+	constexpr float REPLICATION_PERIOD			= 1 / 40.f; // 40Hz
 #endif
 	constexpr uint16_t MAX_PACKET_SIZE		= 1'500 /* MTU */ - 20 /* IP Header */ - 8 /* UDP Header */;
 	constexpr float PACKET_TIME_OUT			= 0.5f;
